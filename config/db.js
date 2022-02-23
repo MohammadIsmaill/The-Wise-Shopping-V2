de596219d219cc922a2bd24528e81eec76b5d821
MohammadIsmaill <mohammad2002ismail@gmail.com>
2022-02-23 18:12:00 +0200

    Initial commit

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectMongo = () => {
-  mongoose.connect(process.env.DB_URL)
+  mongoose.connect('mongodb://localhost/myapp' || process.env.DB_URL)
 
   const db = mongoose.connection
   db.on('error', console.error.bind(console, 'connection error: '))
